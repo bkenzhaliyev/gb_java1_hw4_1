ceaa98891cbb587ef04276d87e0dd5d74341fa92
bkenzhaliyev <bkenzhaliyev@gmail.com>
2021-06-29 19:21:14 +0600

    Initial commit

---
+++ src/hw4_1/hw4_1.java
@@ -0,0 +1,21 @@
+package hw4_1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class hw4_1 {
+    public static char[][] map;
+    public static final int SIZE = 5;
+    public static final int DOTS_TO_WIN = 4;
+
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+
+    public static void main(String[] args) {
+
+    }
+
+}
